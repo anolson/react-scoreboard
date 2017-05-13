bd0fbeae3c6a1037992203aa1329c6e35d6df30b
Andrew Olson <anolson@gmail.com>
2017-05-12 22:09:47 -0400

    Add dependencies with yarn.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app.jsx',
+  output: {
+    path: __dirname + "/build",
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+};
